81354ed047f5a4585c56dfca7bbbdebf41a4c0db
joaonunespaiva <joaonunespaiva@gmail.com>
2023-07-05 21:32:26 +0100

    alteração 2

--- index.js
+++ index.js
@@ -1,24 +1,83 @@
+const { json } = require("express");
 const express = require("express"); // importar o express - previamente instalado
 const app = express(); // criar aplicação do express
 
-app.get("/", (req, res) => {
-  console.log(req.headers);
-  res.send("Hi!");
+//uma variável com uma Array com 3 objetos
+const db = [
+  {
+    id: 1,
+    title: "Horror Movie",
+    genre: "Horror",
+    year: 1999,
+    rentals: 7,
+  },
+  {
+    id: 2,
+    title: "Drama Movie",
+    genre: "Drama",
+    year: 2001,
+    rentals: 300,
+  },
+  {
+    id: 3,
+    title: "Action Movie",
+    genre: "Action",
+    year: 2023,
+    rentals: 7568,
+  },
+];
+
+//CRUD - Create Read Update DELETE
+
+// GET https://api.edit.pt/ => GET /
+// GET https://api.edit.pt/directors => GET /directors
+
+//REST
+
+// GET https://api.edit.pt/movies => GET /movies     (ROTA)
+app.get("/movies", (req, res) => {
+  //res.json(db); //This method sends a JSON response.
+  //a mesma coisa
+  const movies = db.map((m) => {
+    return {
+      title: m.title,
+      genre: m.genre,
+      year: m.year,
+    };
+  });
+  res.status(200).json(movies);
 });
 
+app.get("/movies/:id", (req, res) => {
+  //get ID from request
+  //fetch record from DB
+  //return json
+  const id = parseInt(req.params.id);
+  let movie = null;
 
+  for (let index = 0; index < db.length; index++) {
+    if (id === db[index].id);
+    {
+      movie = db[index];
+      break;
+    }
+  }
+  res.status(200).json(movie);
+});
+
+// Array.find
+// Array.filter --
+// Array.forEach --
+// for (perceber bem o metodo for - estudar)
 
 // app.get("/hello", (req, res) => {
 //   res.send("Hello World!");
 // });
-
 // app.get("/bye", (req, res) => {
 //   res.send("Bye World!");
 // });
-
 // é a mesma coisa que em cima
 // function getRoot(req,res){
-
 // }
 // app.get("/", getRoot)
 
